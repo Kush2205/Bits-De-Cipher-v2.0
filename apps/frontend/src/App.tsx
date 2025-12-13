1d9c4a6e1cbfacb0723e8d3f028bb7a5a60c3e8b
Kushagra Kumar <kr.kushagra2205@gmail.com>
2025-12-13 20:27:13 +0530

    Added UI example for Input

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,9 +1,10 @@
-
+import Input from "./ui/input"
 function App() {
  
   return (
     <>
      <h1 className='text-9xl text-center'>This is the frontend</h1>
+     <Input placeholder="Enter text" variant="large" classname="my-custom-class" />
      </>
   )
 }
