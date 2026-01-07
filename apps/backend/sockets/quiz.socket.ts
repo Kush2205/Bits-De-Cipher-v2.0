be0fefa37ae144f31c8cac55e82ab9ca510b0bd8
Aa5hut0sh <aashutoshsharma2905@gmail.com>
2026-01-08 01:20:19 +0530

    added leaderboard in socket

--- apps/backend/sockets/quiz.socket.ts
+++ apps/backend/sockets/quiz.socket.ts
@@ -16,45 +16,65 @@ export const quizSockets = (io: Server) => {
 
     socket.join("quiz-room");
 
-    socket.on("join_quiz", async () => {
+    socket.on("joinQuiz", async () => {
       try {
         console.log(`User ${userId} joined game-room`);
         const question = await getCurrentQuestion(userId);
         const userStats = await getUserStats(userId);
         const leaderboard = await getGlobalLeaderboard(10);
 
-        socket.emit("quiz:init", {
+        socket.emit("initialData", {
           question,
           userStats,
           leaderboard
         });
       } catch {
-        socket.emit("quiz:error", { message: "Failed to load quiz" });
+        socket.emit("error", { message: "Failed to load quiz" });
       }
     });
 
-    socket.on("request_hint", async ({ questionId, hintNumber }) => {
+    socket.on("requestHint", async ({ questionId, hintNumber }) => {
       try {
         const result = await useHint(userId, questionId, hintNumber);
-        socket.emit("quiz:hint", result);
+        socket.emit("hintData", result);
       } catch {
-        socket.emit("quiz:error", { message: "Hint failed" });
+        socket.emit("error", { message: "Hint failed" });
       }
     });
 
-    socket.on("submit_answer", async ({ questionId, answer }) => {
+    socket.on("submitAnswer", async ({ questionId, answer }) => {
       try {
         const result = await submitAnswer(userId, questionId, answer);
-        socket.emit("quiz:answer_result", result);
+        socket.emit("answerResult", result);
 
         if (result.isCorrect) {
           const leaderboard = await getGlobalLeaderboard(10);
-          io.to("quiz-room").emit("quiz:leaderboard_update" , leaderboard);
+          io.to("quiz-room").emit("leaderboardUpdate" , leaderboard);
         }
       } catch {
-        socket.emit("quiz:error", { message: "Submission failed" });
+        socket.emit("error", { message: "Submission failed" });
       }
     });
+
+    socket.on('requestLeaderboard', async (data: { limit?: number }) => {
+      try {
+        const leaderboard = await getGlobalLeaderboard(data?.limit || 15);
+        socket.emit('leaderboardData', { leaderboard });
+      } catch (error) {
+        socket.emit('error', { message: 'Failed to fetch leaderboard' });
+      }
+    });
+
+    socket.on('requestAllLeaderboard', async () => {
+      try {
+        const leaderboard = await getGlobalLeaderboard();
+        socket.emit('allLeaderboardData', { leaderboard });
+      } catch (error) {
+        socket.emit('error', { message: 'Failed to fetch full leaderboard' });
+      }
+    });
+
+
 
     socket.on("disconnect", () => {
       console.log("Socket disconnected:", userId);
