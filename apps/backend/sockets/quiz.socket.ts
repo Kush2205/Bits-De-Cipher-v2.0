12bde5591aaede087d17f8f1e81b65e07a6db0dc
Vedant Singh <vedantsingh0072@gmail.com>
2026-01-13 00:29:33 +0530

    Rules modal , live leaderboard sync

--- apps/backend/sockets/quiz.socket.ts
+++ apps/backend/sockets/quiz.socket.ts
@@ -33,10 +33,8 @@ class QuizSocket {
                     return;
                 }
                 this.sendInitialData(socket, userStats);
-            });
-
-            
-
+                this.fetchAndEmitLeaderboardData(socket);
+            }); 
         })
     }
 
---
+++ apps/frontend/src/pages/Rules.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Rules=()=>{
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
